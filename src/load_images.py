726de1a13f4be15182bd6a371b250624abc0da08
MikeLrUC <miguelrabuge12@gmail.com>
2021-11-13 23:28:50 +0000

    Added: Resize Script and Resized data

--- src/load_images.py
+++ src/load_images.py
@@ -1,74 +1,14 @@
+import numpy as np
+from PIL import Image
 from os import listdir
 from os.path import isfile
-from PIL import Image
-
-# load and display an image with Matplotlib
-from matplotlib import image
-from matplotlib import pyplot
-import numpy as np
-from numpy.lib.npyio import load
-
-
-'''
-train_dir_path = "/Users/gabriel/Documents/UC/4-ano/1-semestre/IA/PVTL_dataset/train"
-val_dir_path = "/Users/gabriel/Documents/UC/4-ano/1-semestre/IA/PVTL_dataset/val"
-
-vehicle_red = []
-
-labels_dict = {'vehicle-red': 0, 'vehicle-green': 1, 'pedestrian-red': 2, 'pedestrian-green': 3}
-
-lbs = list(labels_dict.keys())
-print(lbs)
-
-### obter imagens
-p = train_dir_path + "/" + lbs[0] + "/"
-images = [p + f for f in listdir(p) if isfile(p + f)]
-#print(images)
-
-
-for img in images:
-    #temp = np.asarray(image.imread(img))
-    temp = np.asarray(Image.open(img))
-    #print(temp)
-    vehicle_red.append(temp)
-
-label_values = [labels_dict[lbs[0]]] * len(images)
-l = [labels_dict[lbs[0]] for _ in range(len(images))]
-
-print(label_values)
-
-
-#print(vehicle_red[0])
-
-pyplot.imshow(Image.fromarray(vehicle_red[0]))
-pyplot.show()
-
-
-
-
-print(labels_dict)
-#im = Image.open(images[0], 'r')
-#im.show()
-
-
-# # load image as pixel array
-# data = image.imread(images[0])
-# # summarize shape of the pixel array
-# print(data.dtype)
-# print(data.shape)
-# # display the array of pixels as an image
-# pyplot.imshow(data)
-# pyplot.show()
-
-'''
 
 def load_images(train_dir_path, val_dir_path):
-    ###
-    #Loads the images in the pvtl subfolders
-    ###
 
-    # train_dir_path = "/Users/gabriel/Documents/UC/4-ano/1-semestre/IA/PVTL_dataset/train"
-    # val_dir_path = "/Users/gabriel/Documents/UC/4-ano/1-semestre/IA/PVTL_dataset/val"
+    ###############################################
+    ### Loads the Images in the PVTL Subfolders ###
+    ###############################################
+
     dir_paths = [train_dir_path, val_dir_path]
     
     labels_dict = {'vehicle-red': 0, 'vehicle-green': 1, 'pedestrian-red': 2, 'pedestrian-green': 3}
@@ -99,20 +39,17 @@ def load_images(train_dir_path, val_dir_path):
                     temp = np.asarray(Image.open(img_p))
                     X_val.append(temp)
                 y_val = y_val + [labels_dict[l] for _ in range(len(image_paths))]
-                    
-        
-    
 
     return X_train, y_train, X_val, y_val
 
 
 
 if __name__ == '__main__':
-    train_dir_path = "../data/PVTL_dataset/train"
-    val_dir_path = "../data/PVTL_dataset/val"
+    DIR = "processed" 
+    train_dir_path = f"../data/{DIR}/train"
+    val_dir_path = f"../data/{DIR}/val"
 
     X_train, y_train, X_val, y_val = load_images(train_dir_path, val_dir_path)
-
     print("n_elements of: X_train - {},  y_train - {}".format(len(X_train), len(y_train)))
     print("n_elements of: X_val - {},  y_val - {}".format(len(X_val), len(y_val)))
 
---
+++ src/resize.py
@@ -0,0 +1,57 @@
+import os
+import cv2 # OpenCV lib for image manipulation
+
+RAW_ROOT = "../data/PVTL_dataset/"
+PCD_ROOT = "../data/processed/"
+TRAIN_PATH = "train/"
+VAL_PATH = "val/"
+
+def get_image_names(path_type):
+    return [f"{path_type}{img_class}/{image}" for img_class in os.listdir(path_type) for image in os.listdir(f"{path_type}{img_class}")]
+
+def get_sizes(images):
+    widths, heights = [], []
+    for image in images:
+        img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+        widths.append(img.shape[1])
+        heights.append(img.shape[0])
+    return widths, heights
+
+def resize_and_pad_images(images, w_target, h_target):
+    for image in images:
+        # Image
+        img = cv2.imread(image, cv2.IMREAD_UNCHANGED)
+        
+        # Getting minimum ratio (to maintain image format)
+        ratio_w = w_target / img.shape[1]
+        ratio_h = h_target / img.shape[0]
+        desired_ratio = min(ratio_w, ratio_h)
+        
+        # Resizing
+        desired_size = [int(desired_ratio * img.shape[0]), int(desired_ratio * img.shape[1])]
+        resized = cv2.resize(img, (desired_size[1], desired_size[0]))
+        
+        # Padding and Centering Image
+        delta_w = w_target - desired_size[1]
+        delta_h = h_target - desired_size[0]
+        top, bottom = delta_h // 2, delta_h - (delta_h // 2)
+        left, right = delta_w // 2, delta_w - (delta_w // 2)
+        padded = cv2.copyMakeBorder(resized, top, bottom, left, right, cv2.BORDER_CONSTANT, value=[0,0,0])
+        
+        # Writing Image to Processed Path
+        cv2.imwrite(PCD_ROOT + image[len(RAW_ROOT):], padded)
+
+if __name__ == "__main__":
+    # Getting Training and Validation Images
+    train_images = get_image_names(RAW_ROOT + TRAIN_PATH)
+    validation_images = get_image_names(RAW_ROOT + VAL_PATH)
+    total_images = train_images + validation_images
+
+    # Getting Maximum sizes
+    widths, heights = get_sizes(total_images)
+    w_max, h_max = max(widths), max(heights)
+
+    # Resizing and Padding Images to maximum Width and Height, maintaining picture ratio
+    resize_and_pad_images(total_images, w_max, h_max)
+    print(f"Resized and Padded Images to w,h = ({w_max}, {h_max})") # (388, 884)
+    
